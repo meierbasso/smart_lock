4e68c67c17235f6974492af93a08833290193c1c
meierbasso <114088975+meierbasso@users.noreply.github.com>
2023-02-11 20:11:45 -0300

    Update smart_lock_BLE-scan-server.ino

--- smart_lock_BLE-scan-server.ino
+++ smart_lock_BLE-scan-server.ino
@@ -1,10 +1,9 @@
 /*
    Smart Lock PoC: Lock and unlock based on smartphone or smartwatch proximity.
-   
-   THIS IS NOT READY YET! WORK IN PROGRESS...
-   
-   The BLE Server implementation and the BLE Scanner implementation was based on Neil Kolban examples.
+   Developed by Carlos Alberto Meier Basso
+   The BLE Server implementation and the BLE Scanner implementation was based on Neil Kolban examples adapted to ESP32 by Evandro Copercini.
 */
+
 
 #include <BLEDevice.h>
 #include <BLEUtils.h>
@@ -12,6 +11,13 @@
 #include <BLEAdvertisedDevice.h>
 #include <BLEServer.h>
 
+// if using Servo: Include the ESP32 Arduino Servo Library instead of the original Arduino Servo Library
+#include <ESP32Servo.h> 
+int servoPin = 18; //GPIO to attach servo control 
+Servo myservo;  // create servo object to control a servo
+const bool useServo = false; //if using a servo to lock or unlock
+int solenoidPin = 17; //GPIO to attach solenoid control 
+const bool useSolenoid = true; //if using a solenoid to lock or unlock
 
 //Server definitions and vars
 // See the following for generating UUIDs:
@@ -20,7 +26,6 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 bool deviceConnected = false;
 char remoteAddress[18];
-
 
 //scan definitions and vars
 const int MAX_KEYS = 3;
@@ -32,11 +37,12 @@ BLEScan* pBLEScan;
 
 //periferrals definitions and vars
 const int buttonPin = 14;
-const int ledPin = 4; //show if door is locked
+const int ledPin = 4; //LED that show if door is locked
 int buttonState = 0;
 const bool useHall = false; //if using hall sensor to verify if the door is closed, use "true"
+bool locked = false; //status if the door is locked or not
 
-// Server calback
+//server calback 
 //Setup callbacks onConnect and onDisconnect
 class MyServerCallbacks: public BLEServerCallbacks { //if more info needes, visit: https://randomnerdtutorials.com/esp32-ble-server-client/
 	void onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) {
@@ -68,8 +74,7 @@ class MyServerCallbacks: public BLEServerCallbacks { //if more info needes, visi
 //callback do scan
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-      for (int i = 0; i < (sizeof(knownBLEAddresses) / sizeof(knownBLEAddresses[0])); i++)
-      {
+      for (int i = 0; i < (sizeof(knownBLEAddresses) / sizeof(knownBLEAddresses[0])); i++) {
         //Uncomment to Enable Debug Information
         //Serial.println("*************Start**************");
         //Serial.println(sizeof(knownBLEAddresses));
@@ -78,10 +83,9 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         //Serial.println(advertisedDevice.getAddress().toString().c_str());
         //Serial.println(knownBLEAddresses[i].c_str());
         //Serial.println("*************End**************");
-        if (strcmp(advertisedDevice.getAddress().toString().c_str(), knownBLEAddresses[i].c_str()) == 0)
-        {
+        if (strcmp(advertisedDevice.getAddress().toString().c_str(), knownBLEAddresses[i].c_str()) == 0) {
           device_found = true;
-          Serial.println("*************FOUND!**************");
+          Serial.println("Dispositivo chave detectado!");
           break;
         }
         else
@@ -95,7 +99,7 @@ void addKey() { //adds a new key to the keys array
   bool added = false;
   for (int i = 0; i < MAX_KEYS; i++) {  
     if (!added && knownBLEAddresses[i].c_str() == "") {
-       knownBLEAddresses[i] = remoteAddress; // <<<<<<<<converter para string????
+       knownBLEAddresses[i] = remoteAddress; 
        added=true;
     }
     else if (knownBLEAddresses[i].c_str() == remoteAddress) {
@@ -104,20 +108,6 @@ void addKey() { //adds a new key to the keys array
       }
       added == true; //key already added
     }
-    /*if (!added) {
-      if (knownBLEAddresses[i].c_str() == "" ) {
-        knownBLEAddresses[i] = remoteAddress;  //<<<<<<<<converter para string????
-        added = true;
-      }
-      else if (knownBLEAddresses[i].c_str() == remoteAddress) {
-        added  = true;
-      }
-    else {
-      if (knownBLEAddresses[i].c_str() == remoteAddress) { //if key already added, remove it to avoid key duplicity
-          knownBLEAddresses[i] = "";
-      }
-    }
-    */
   }
   if (added) {//debug only
     Serial.printf("Key %s added successfully!", remoteAddress);
@@ -136,11 +126,27 @@ bool doorClosed() { //verify if the Hall sensor should be used and verify if the
   return true; //if not considering Hall sensor, returns true, as the door is closed.
 }
 
+void lockByServo() { //lock the door using a servo
+  for (int pos = 0; pos <= 180; pos += 1) {
+    // in steps of 1 degree
+    myservo.write(pos);
+    delay(15); // waits 15ms to reach the position
+  }
+}
+void unlockByServo() { //unlock the door using a servo
+  for (int pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(15); // waits 15ms to reach the position
+  }                   
+}
+
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(solenoidPin, OUTPUT);
 
 /////server
   //create BLE device
@@ -156,17 +162,15 @@ void setup() {
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  pCharacteristic->setValue("Esse é o valor dado a característica...");
+  pCharacteristic->setValue("Characteristic");
   //start service and advertising 
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
 
 /////scanner
   Serial.println("Scanning...");
@@ -177,6 +181,10 @@ void setup() {
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);  // less or equal setInterval value
+
+//////servo
+  myservo.setPeriodHertz(50);// Standard 50hz servo
+  myservo.attach(servoPin, 500, 2400);   // attaches the servo on pin 18
 }
 
 void loop() {
@@ -198,28 +206,33 @@ void loop() {
     //int rssi = device.getRSSI();
     //Serial.print("RSSI: ");
     //Serial.println(rssi);
-    if (/*rssi > RSSI_THRESHOLD &&*/ device_found == true && doorClosed())   {
+
+    //Proceed Unlocking
+    if (/*rssi > RSSI_THRESHOLD &&*/ device_found == true && locked && doorClosed())   {
       Serial.println("Unlock the Door!");
       digitalWrite(LED_BUILTIN, HIGH);
       digitalWrite(ledPin, LOW);
+      if (useServo) unlockByServo();
+      if (useSolenoid) digitalWrite(solenoidPin, HIGH);
+      locked = false;
       delay(5000);
+      if (useSolenoid) { //solenoid shouldn't be HIGH for more than 5 seconds. If needed, it will repeat the process.
+        digitalWrite(solenoidPin, LOW);
+        locked = true;
+      }
     }
   }
   
-  if (!device_found && doorClosed()) {
+  //Proceed Locking
+  if (!device_found && !locked && doorClosed()) {
       Serial.println("Lock the door");      
       digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(ledPin, HIGH);      
+      digitalWrite(ledPin, HIGH); 
+      if (useServo) 
+        lockByServo();  
+      digitalWrite(solenoidPin, LOW);
+      locked = true;   
   }
   delay(2000);
   pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
-
-
-
-  /**Serial.print("Devices found: ");
-  Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!");
-  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
-  delay(2000);
-  */
 }
